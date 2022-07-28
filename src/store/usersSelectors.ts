3b80f3eb48dbe3c25792a37a616630582471a48b
suveiko <suveikosasha@gmail.com>
2022-07-28 15:43:57 +0400

    feat: add createSelector to usersSelectors

--- src/store/usersSelectors.ts
+++ src/store/usersSelectors.ts
@@ -1,9 +1,22 @@
 import {StoreType} from "./store";
 
+import {createSelector} from "reselect";
 
-export const getUsers = (state: StoreType) => state.usersPage.users
-export const getPageSize = (state: StoreType) => state.usersPage.pageSize
-export const getTotalUserCount = (state: StoreType) => state.usersPage.totalUsersCount
-export const getCurrentPage = (state: StoreType) => state.usersPage.currentPage
-export const getIsFetching = (state: StoreType) => state.usersPage.isFetching
-export const getFollowingInProgress = (state: StoreType) => state.usersPage.followingInProgress
+
+export const getUsersSelector = (state: StoreType) => state.usersPage.users
+export const getUsers = createSelector(getUsersSelector, users => users)
+
+export const getPageSizeSelector = (state: StoreType) => state.usersPage.pageSize
+export const getPageSize = createSelector(getPageSizeSelector, pageSize => pageSize)
+
+export const getTotalUserCountSelector = (state: StoreType) => state.usersPage.totalUsersCount
+export const getTotalUserCount = createSelector(getTotalUserCountSelector, totalUsersCount => totalUsersCount)
+
+export const getCurrentPageSelector = (state: StoreType) => state.usersPage.currentPage
+export const getCurrentPage = createSelector(getCurrentPageSelector, currentPage => currentPage)
+
+export const getIsFetchingSelector = (state: StoreType) => state.usersPage.isFetching
+export const getIsFetching = createSelector(getIsFetchingSelector, isFetching => isFetching)
+
+export const getFollowingInProgressSelector = (state: StoreType) => state.usersPage.followingInProgress
+export const getFollowingInProgress = createSelector(getFollowingInProgressSelector, followingInProgress => followingInProgress)
