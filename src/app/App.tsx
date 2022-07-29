b0da222cf6c7b1f870dd7ee6774e81fb8b1298c9
suveiko <suveikosasha@gmail.com>
2022-07-29 15:39:54 +0400

    refactor: App from class to functional component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,7 @@
-import {Component, FC} from "react";
-import {compose} from "redux";
-import {connect} from "react-redux";
+import {useEffect} from "react";
 import {Route, withRouter} from "react-router-dom";
+
+import {useAppDispatch} from "../hooks/hooks";
 
 import Login from "../components/body/Login/Login";
 import Profile from "../components/body/Profile/Profile";
@@ -11,48 +11,37 @@ import Header from "../components/header/Header";
 import Dialogs from "../components/body/Dialogs/Dialogs";
 
 import {initializeApp} from "../store/appReducer/appReducer";
-import {getAppInitialized} from "../store/appReducer/appSelectors";
-import {StoreType} from "../store/store";
 
 import Preloader from "../components/common/Preloader/Preloader";
 
 import './App.css';
 
 
-type AppType = typeof mapDispatchToProps & ReturnType<typeof mapStateToProps>
+function App() {
+    const dispatch = useAppDispatch()
+
+    useEffect(() => {
+        dispatch(initializeApp())
+    }, [dispatch])
 
 
-class App extends Component<AppType> {
-    componentDidMount() {
-        this.props.initializeApp()
+    if (!initializeApp) {
+        return <Preloader/>
     }
 
-    render() {
-        if (!this.props.initializeApp) {
-            return <Preloader/>
-        }
-        return (
-            <div className='app-wrapper'>
-                <Header/>
-                <Navbar/>
-                <div className='app-wrapper-content'>
-                    <Route path='/dialogs' render={() => <Dialogs/>}/>
-                    <Route path='/profile/:userId?' render={() => <Profile/>}/>
-                    <Route path='/users' render={() => <Users/>}/>
-                    <Route path='/login' render={() => <Login/>}/>
-                </div>
+    return (
+        <div className='app-wrapper'>
+            <Header/>
+            <Navbar/>
+            <div className='app-wrapper-content'>
+                <Route path='/dialogs' render={() => <Dialogs/>}/>
+                <Route path='/profile/:userId?' render={() => <Profile/>}/>
+                <Route path='/users' render={() => <Users/>}/>
+                <Route path='/login' render={() => <Login/>}/>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
-const mapStateToProps = (state: StoreType) => ({
-    state: getAppInitialized(state)
-})
-const mapDispatchToProps = {initializeApp}
 
-
-export default compose<FC>(
-    connect(mapStateToProps, mapDispatchToProps),
-    withRouter
-)(App)
+export default withRouter(App)
